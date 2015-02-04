d4b7076db98b7f2e4f329048413e656a1068bf86
grindv1k <torstein.grindvik@gmail.com>
2015-02-04 13:47:34 +0100

    Added functions to hw.h

--- source/hw.h
+++ source/hw.h
@@ -1,14 +1,39 @@
 
-void hw_closeDoors();
-void hw_openDoors();
 
+// Elevator movement
 void hw_elevMoveDownwards();
 void hw_elevMoveUpwards();
 void hw_elevStop();
 
-// Returns a value 1-4 indicating which floor the elevator currently is on.
-// If the elevator is between floors, returns 0
-int hw_getCurrentFloor();
+// Current floor status
+int hw_getCurrentFloor(); 				// Returns a value 0-3 indicating which floor the elevator currently is on.
+							// If the elevator is between floors, returns -1
 
-// Returns 1-4 depending on floor wanted
-int hw_elevGetFloorButton();
+// Checks button statii
+int hw_getCallUpButtonStatus(int floor);		// Returns 1 if the call up button is pressed, else 0
+int hw_getCallDownButtonStatus(int floor);		// Returns 1 if the call down button is pressed, else 0
+int hw_getStopButtonStatus();				// Returns 1 if the stop button is pressed, else 0
+int hw_getInsideButtonStatus(int floor);		// Returns 1 if the inside button is pressed, else 0
+
+// LAMPS
+void hw_setInsideButtonLampOn(int); 			// Int from 0-3, turns on lamp for corresponding button int
+void hw_setInsideButtonLampOff(int); 			// Int from 0-3, turns off lamp for corresponding button int
+
+void hw_setCallUpLampOn(int floor);			// Int 0-3, dir, turns on lamp for an up arrow button
+void hw_setCallUpLampOff(int floor);			// Int 0-3, dir, turns off lamp for an up arrow button
+void hw_setCallDownLampOn(int floor);			// Int 0-3, dir
+void hw_setCallDownLampOff(int floor);			// Int 0-3, dir
+void hw_setCallUpDownButtonLampsOff();			// Turns off lamps for all arrow buttons
+
+void hw_setFloorButtonLampsOff();			// Turns off all floor button lamps (not floor indicator)
+void hw_setFloorButtonLampOn(int floor);		// Turns on a floor button lamp
+void hw_setFloorButtonLampOff(int floor);		// 
+
+void hw_setLastFloorLamp(int floor);			// Holds last floor's lamp lit (indicator)
+
+void hw_setStopLampOn();
+void hw_setStopLampOff();
+
+// Doors
+void hw_closeDoors();					// Elevator door lamp on 
+void hw_openDoors();					// off
