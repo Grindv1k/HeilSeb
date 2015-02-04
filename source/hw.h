d1bacbb33636b870c3252c4c9d4e0c20952f4311
grindv1k <torstein.grindvik@gmail.com>
2015-02-04 12:44:04 +0100

    added function to hw.h

--- source/hw.h
+++ source/hw.h
@@ -9,3 +9,6 @@ void hw_elevStop();
 // Returns a value 1-4 indicating which floor the elevator currently is on.
 // If the elevator is between floors, returns 0
 int hw_getCurrentFloor();
+
+// Returns 1-4 depending on floor wanted
+int hw_elevGetFloorButton();
