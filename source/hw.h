f0cdc6e4d720fbf96c9ab222d70fdf65b62cd669
Asbjørn Espe <azzi777@gmail.com>
2015-02-04 11:45:04 +0100

    Added project files

---
+++ source/hw.h
@@ -0,0 +1,11 @@
+
+void hw_closeDoors();
+void hw_openDoors();
+
+void hw_elevMoveDownwards();
+void hw_elevMoveUpwards();
+void hw_elevStop();
+
+// Returns a value 1-4 indicating which floor the elevator currently is on.
+// If the elevator is between floors, returns 0
+int hw_getCurrentFloor();
