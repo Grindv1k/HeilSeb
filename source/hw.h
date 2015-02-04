200c00bf0bb3bd848f21f74bb08eaec4af520748
Asbjørn Espe <azzi777@gmail.com>
2015-02-04 12:44:38 +0100

    aded somting

--- source/hw.h
+++ source/hw.h
@@ -6,6 +6,6 @@ void hw_elevMoveDownwards();
 void hw_elevMoveUpwards();
 void hw_elevStop();
 
-// Returns a value 1-4 indicating which floor the elevator currently is on.
-// If the elevator is between floors, returns 0
+// Returns a value 0-3 indicating which floor the elevator currently is on.
+// If the elevator is between floors, returns -1
 int hw_getCurrentFloor();
