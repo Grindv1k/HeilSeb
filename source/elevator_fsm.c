d62de5029034dbbd4e8443739051eeddc6d0c64e
Asbjørn Espe <azzi777@gmail.com>
2015-02-09 11:00:43 +0100

    d

--- source/elevator_fsm.c
+++ source/elevator_fsm.c
@@ -4,4 +4,19 @@
 #include "elev.h"
 
 int lastFloor;
+
 elev_motor_direction_t currentDirection;
+
+elev_door_status_t doorStatus;
+
+elev_state_t currentState;
+
+void fsm_evInit()
+{
+    elev_init();
+    elev_set_door_open_lamp();
+
+    currentDirection = DIRN_DOWN;
+    elev_set_motor_direction(currentDirection);
+}
+
--- source/main.c
+++ source/main.c
@@ -7,5 +7,6 @@
 
 int main() 
 {
+    fsm_evInit();
 	return 0;
 }
