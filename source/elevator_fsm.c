d301d3831efdec771e13ff05a3c4b99e6c6d9ad4
Asbjørn Espe <azzi777@gmail.com>
2015-02-11 13:34:58 +0100

    Fixed bug where you could request while stop button was pressed

--- source/elevator_fsm.c
+++ source/elevator_fsm.c
@@ -75,7 +75,8 @@ void fsm_evStopButtonReleased()
 
 void fsm_evRequestButtonRegistered(int floor, elev_button_type_t buttonType)
 {
-    if(currentState = ELEV_INIT)
+    if(currentState == ELEV_INIT || 
+        currentState == EMERGENCY_STOP_ON_FLOOR || currentState == EMERGENCY_STOP_BETWEEN_FLOORS)
     {
         return;
     }   
